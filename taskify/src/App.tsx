2caf217ca9c58c8e97e7a74c7d998daeaec6b1c3
GG-Pharaoh <oxdiablo20@gmail.com>
2025-05-17 02:52:00 -0700

    Initial set up done, Code working

---
+++ taskify/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+
+
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <span className="Heading"> Taskify </span>
+
+     
+    </div>
+  );
+}
+
+export default App;
